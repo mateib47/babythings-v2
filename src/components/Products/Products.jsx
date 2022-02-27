898f1deeba9650243e4162383b525f2e1d94c721
mateib47 <m.bucur@student.utwente.nl>
2022-02-27 23:32:58 +0100

    add commerce.js and start implementing shopping cart

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,14 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    {id:1, name: 'Clothing box', description: 'Clothing essentials', price:'$43', image:'https://drive.google.com/uc?export=view&id=1rcf0Qsbkj0vgNDRM9-qeniNWN1qpeprU'},
-    {id:2, name: 'Toys box', description: 'Age specific toys', price:'$23', image:'https://drive.google.com/uc?export=view&id=1K76Fdr4lh1RIZJXuNl-12-FoPWcEujAR'},
-    {id:3, name: 'Hygene box', description: 'All care products you need', price:'$32', image:'https://drive.google.com/uc?export=view&id=16f3_7aezwu89g-oSaKvuFxSczRNyP-GB'}
-
-]
-
-const Products = () => {
+const Products = ({products, onAddToCart}) => {
     const classes = useStyles();
     return (
         <main className={classes.content}>
@@ -19,7 +12,7 @@ const Products = () => {
             <Grid container justifyContent="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
