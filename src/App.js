79db0bcf7fbc6f61d212bf274f215e74d2f65c60
mateib47 <m.bucur@student.utwente.nl>
2022-02-28 18:47:47 +0100

    add cart logic; add checkout stepper

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {commerce} from './lib/commerce';
-import {Products, Navbar, Cart} from './components';
+import {Products, Navbar, Cart, Checkout} from './components';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 const App = () => {
@@ -17,9 +17,24 @@ const App = () => {
     }
 
     const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
-        setCart(item.cart);
+        const {cart} = await commerce.cart.add(productId, quantity);
+        setCart(cart);
     }
+
+    const handleCartQty = async (productId, quantity) => {
+        const {cart} = await commerce.cart.update(productId, {quantity});
+        setCart(cart);
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const {cart} = await commerce.cart.remove(productId);
+        setCart(cart);
+    }    
+
+    const handleEmptyCart = async () => {
+        const {cart} = await commerce.cart.empty();
+        setCart(cart);
+    } 
 
     useEffect(() => {
         fetchProducts();
@@ -36,7 +51,15 @@ const App = () => {
             <Navbar totalItems={cart.total_items}/>
             <Routes>
                 <Route path="/" element={<Products products={products} onAddToCart={handleAddToCart}/>} />
-                <Route path="/cart" element={<Cart cart={cart} />} />
+                <Route path="/cart" element={
+                    <Cart 
+                        cart={cart}
+                        handleEmptyCart={handleEmptyCart}
+                        handleRemoveFromCart={handleRemoveFromCart}
+                        handleCartQty={handleCartQty} 
+                    />} 
+                />
+                <Route path="/checkout" element={<Checkout/>} />
             </Routes>
         </div>
       </Router>
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -3,8 +3,9 @@ import { Typography, Button, Card, CardActions, CardContent, CardMedia} from '@m
 
 import useStyles from './styles';
 
+//todo use react context for props
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
   
   const classes = useStyles(); 
 
@@ -17,12 +18,12 @@ const CartItem = ({ item }) => {
         </CardContent>  
         <CardActions className={classes.cardActions}>
             <div className={classes.buttons}>
-                <Button type="button" size="small">-</Button>
+                <Button type="button" size="small" onClick={() => onUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
                 <Typography>{item.quantity}</Typography>
-                <Button type="button" size="small">+</Button>
+                <Button type="button" size="small" onClick={() => onUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
 
             </div>
-            <Button variant="contained" type="button" color="secondary">Remove</Button>
+            <Button variant="contained" type="button" color="secondary" onClick={() => onRemoveFromCart(item.id)}>Remove</Button>
         </CardActions>
     </Card>
   )
