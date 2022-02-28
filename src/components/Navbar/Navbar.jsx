35d3c35a35de7e64d9e781f4e051685f0f1841f9
mateib47 <m.bucur@student.utwente.nl>
2022-02-28 13:10:33 +0100

    implement routing and cart items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
 import {ShoppingCart} from '@material-ui/icons';
+import {Link, useLocation} from 'react-router-dom';
 
 import logo from '../../assets/img/logo.png';
 import useStyles from './styles';
@@ -9,22 +10,25 @@ import useStyles from './styles';
 
 const Navbar = ({totalItems}) => {
   const classes = useStyles(); 
+  const location = useLocation();
+
   return (
     <>
-        <AppBar position="fixed" className={classes.appBar} color="inheret">
+        <AppBar position="fixed" className={classes.appBar} color="inherit">
             <Toolbar>
-                <Typography variant="h6" className={classes.title} color="inheret">
+                <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                     <img src={logo} alt="BabythingsStore" height="25px" className={classes.image} />
                     Kiekebox
                 </Typography>
                 <div className={classes.grow} />
+                {location.pathname === '/' && (
                 <div className={classes.button}>
-                    <IconButton area-label="Show cart items" color="inheret">
+                    <IconButton component={Link} to="/cart" area-label="Show cart items" color="inherit">
                         <Badge badgeContent={totalItems} color="secondary">
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
-                </div>
+                </div> )}
             </Toolbar>
         </AppBar>
     </>
