b8da7b8cfd7d306e49f2125e3c4e01f7267ca7ce
mateib47 <m.bucur@student.utwente.nl>
2022-03-06 23:36:48 +0100

    fix homepage display

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import useStyles from './styles';
+import {Typography} from '@material-ui/core'
+
+
 
 const Home = () => {
+    const classes = useStyles();
+
   return (
-    <div>Home page</div>
+      <>
+        <div className={classes.toolbar}/>
+        <Typography variant='h6'>Unique baby box subscriptions</Typography>
+      </>
   )
 }
 
