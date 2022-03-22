9e013b40d55a93ddac2bac979645fb8e0be8b411
mateib47 <m.bucur@student.utwente.nl>
2022-03-22 21:46:32 +0100

    add render clothing size based on age

--- src/components/CheckoutForm/BabyForm.jsx
+++ src/components/CheckoutForm/BabyForm.jsx
@@ -19,10 +19,36 @@ import CustomSelect from './CustomSelect' //test this if it works
 
 
 const BabyForm = ({checkoutToken, next}) => {
-    const [babySize, setbabySize] = useState('');
+    const [babyBirthday, setBabyBirthday] = useState('');
+    const [babySize, setBabySize] = useState('');
+
     const methods = useForm();
-    const sizes = checkoutToken.extra_fields.find(x => x.id == 'extr_VPvL5zWGQlAQkX').options.map(y => {return {'label':y, 'id':1}}) ;
+    const sizes = checkoutToken.extra_fields.find(x => x.id == 'extr_VPvL5zWGQlAQkX')
+        .options.map(function(elem, index, arrayobj){return {'label':elem, 'id':index}}) ;
     const [value, setValue] = React.useState(null);
+    console.log(sizes)
+
+    useEffect(() => {
+        if(babyBirthday){
+            if(babyBirthday == -1){
+                //throw some error
+            }else if(babyBirthday <= 3){
+                setBabySize(0);
+            }else if(babyBirthday > 3 && babyBirthday <= 6){
+                setBabySize(1);
+            }else if(babyBirthday > 6 && babyBirthday <= 9){
+                setBabySize(2);
+            }else if(babyBirthday > 9 && babyBirthday <= 12){
+                setBabySize(3);
+            }else if(babyBirthday > 12 && babyBirthday <= 18){
+                setBabySize(4);
+            }else if(babyBirthday > 18 && babyBirthday <= 24){
+                setBabySize(5);
+            }else{
+                //baby too big error
+            }
+        }
+    }, [babyBirthday]);
 
 
   return (
@@ -36,11 +62,18 @@ const BabyForm = ({checkoutToken, next}) => {
                 <Grid item xs={12} sm={6}>
                     <LocalizationProvider dateAdapter={AdapterDateFns}>
                         <DatePicker required
+                            onChange={(e) => {
+                                let months;
+                                let today = new Date()
+                                months = (today.getFullYear() - e.getFullYear()) * 12;
+                                months -= e.getMonth();
+                                months += today.getMonth();
+                                months = months <= 0 ? -1 : months 
+                                setBabyBirthday(months);                             
+                                setValue(e);
+                            }}
                             label="Baby's birthday"
                             value={value}
-                            onChange={(newValue) => {
-                            setValue(newValue);
-                            }}
                             renderInput={(params) => <TextField {...params} />}
                         />
                     </LocalizationProvider>
@@ -55,7 +88,7 @@ const BabyForm = ({checkoutToken, next}) => {
                         <FormControlLabel value="english" control={<Radio />} label="English" />
                     </RadioGroup>
                 </Grid>
-                <CustomSelect name="size" label="Baby's current clothing size" array={sizes} value={babySize} setFunction={setbabySize}/>
+                <CustomSelect name="size" label="Baby's current clothing size" array={sizes} value={babySize} setFunction={setBabySize}/>
                 <Grid item xs={12} sm={6}>
                     <FormControlLabel control={<Checkbox/>} label="Gift" />
                 </Grid> 
