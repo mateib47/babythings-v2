5fc0ebb1fc6e7086eed339b02fa2cd8f063bad24
mateib47 <m.bucur@student.utwente.nl>
2022-04-27 07:29:18 +0200

    fix popup not closing after click

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,62 +1,60 @@
-import * as React from 'react';
+import * as React from "react";
 import useStyles from "./styles";
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import { useEffect } from 'react';
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { useEffect } from "react";
 import { Container, Typography, Grid, Box } from "@material-ui/core";
-import {ContactDefault, ContactCustomer, ContactBusiness} from './ContactComponents';
-
+import {
+  ContactDefault,
+  ContactCustomer,
+  ContactBusiness,
+} from "./ContactComponents";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
-  });
-  
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Contact = () => {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-    const [user, setUser] = React.useState(false);
-    let contactPage;
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const [user, setUser] = React.useState(false);
+  let contactPage;
 
-    switch(user){
-      case "customer":
-        contactPage = <ContactCustomer />;
-        break;
-      case "business":
-        contactPage = <ContactBusiness />;
-        break;
-      default:
-        contactPage = <ContactDefault />;
-    }
+  switch (user) {
+    case "customer":
+      contactPage = <ContactCustomer />;
+      break;
+    case "business":
+      contactPage = <ContactBusiness />;
+      break;
+    default:
+      contactPage = <ContactDefault />;
+  }
 
-    useEffect(() => {
-        handleClickOpen();
-      });
-
-    // useEffect(() => {
-    //   handleClose();
-    // }, [user])
-    
+  // useEffect(() => {
+  //   handleClose();
+  // }, [user])
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const handleClose = (u) => {
+  const handleClose = (e, u) => {
+    e.preventDefault();
     setOpen(false);
     setUser(u);
-    console.log(open)
+    console.log(open);
   };
 
   return (
     <div className={classes.root}>
-        <div className={classes.toolbar} />
-        <Typography>Contact us</Typography>
+      <div className={classes.toolbar} />
+      <Typography>Contact us</Typography>
       <Dialog
         open={open}
         TransitionComponent={Transition}
@@ -64,20 +62,23 @@ const Contact = () => {
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle>{"Are you interested in buying or collaborating with us?"}</DialogTitle>
+        <DialogTitle>
+          {"Are you interested in buying or collaborating with us?"}
+        </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-slide-description">
-            Let us know if you would like to get notified when our boxes are in stock or if you are a business and you want to get in touch.
+            Let us know if you would like to get notified when our boxes are in
+            stock or if you are a business and you want to get in touch.
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => handleClose("customer")}>Customer</Button>
-          <Button onClick={() => handleClose("business")}>Business</Button>
+          <Button onClick={(e) => handleClose(e, "customer")}>Customer</Button>
+          <Button onClick={(e) => handleClose(e, "business")}>Business</Button>
         </DialogActions>
       </Dialog>
       {contactPage}
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
